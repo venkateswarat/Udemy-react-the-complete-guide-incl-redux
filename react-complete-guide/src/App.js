c875fc22bf80dc6a45e18129a88ba83ef3a43694
venkatfiniteloop <64524789+venkatfiniteloop@users.noreply.github.com>
2021-03-05 17:17:08 +0530

    now fixed h1 issue with React Element

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -10,7 +10,8 @@ import React,{Component} from 'react';
 
 class App extends Component{
   render(){
-    return React.createElement('div', null,'h1','Hello World123')
+    // return React.createElement('div', null,'h1','Hello World123')
+    return React.createElement('div', null,React.createElement('h1',null,'Hello World123'))
   }
 }
 export default App;
