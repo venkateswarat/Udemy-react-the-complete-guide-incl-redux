56c274cd71224d247d98b954b31294dcbcba8542
venkatfiniteloop <64524789+venkatfiniteloop@users.noreply.github.com>
2021-03-05 22:24:43 +0530

    Inline Styling

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -25,10 +25,19 @@ class App extends Component{
   }
 
   render(){
+
+    const style={
+      backgroundColor:'white',
+      font:'inherit',
+      border:'1px solid blue',
+      padding:'8px',
+      cursor:'pointer'
+    }
+
     return (<div className="App">
       Hello World
       <br/>
-      <button onClick={()=>{this.swichNameHandler("app1")}}>click me</button>
+      <button style={style} onClick={()=>{this.swichNameHandler("app1")}}>click me</button>
       <Person changed={this.nameChangedHandler} name={this.state.persons[0].name} age={this.state.persons[0].age}>a</Person>
       <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.swichNameHandler.bind(this,'person')}/>
       <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
