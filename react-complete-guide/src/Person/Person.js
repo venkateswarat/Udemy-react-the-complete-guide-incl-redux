64a9af06b0c8985902ee59d8cb6aadb741294e74
venkatfiniteloop <64524789+venkatfiniteloop@users.noreply.github.com>
2021-03-05 22:19:16 +0530

    Styling Person component

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import './Person.css'
 const person =(props)=>{
-    return <div>
+    return <div className="Person">
                 <p onClick={props.click}>I am {props.name}. {props.age}  Years. addition {1+2}</p>
                 <p>{props.children}</p>
                 <input type="text" onChange={props.changed} value={props.name}/>
