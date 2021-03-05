bbc2d8d604251cd134ebac93e1c662cef634a747
venkatfiniteloop <64524789+venkatfiniteloop@users.noreply.github.com>
2021-03-05 17:52:27 +0530

    setting dynamic properties

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -7,9 +7,9 @@ class App extends Component{
   render(){
     return (<div className="App">
       Hello World
-      <Person/>
-      <Person/>
-      <Person/>
+      <Person name="a" age="1">a</Person>
+      <Person name="b" age="2"/>
+      <Person name="c" age="3"/>
     </div>
     )
   }
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,5 +1,5 @@
 import React from 'react';
-const person =()=>{
-    return <p>I am a Person. {Math.floor(Math.random()*30)}  Years. addition {1+2}</p>
+const person =(props)=>{
+    return <p>I am {props.name}. {props.age}  Years. addition {1+2}</p>
 }
 export default person;
