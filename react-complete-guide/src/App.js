2c2f278f4fc36668198d8aec7f6a271169e42ee0
venkatfiniteloop <64524789+venkatfiniteloop@users.noreply.github.com>
2021-03-05 20:12:24 +0530

    Using the state property

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -4,12 +4,21 @@ import Person from './Person/Person'
 
 
 class App extends Component{
+  state={
+    persons:[
+      {name:'a',age:28},
+      {name:'b',age:29},
+      {name:'c',age:30}
+    ]
+  }
   render(){
     return (<div className="App">
       Hello World
-      <Person name="a" age="1">a</Person>
-      <Person name="b" age="2"/>
-      <Person name="c" age="3"/>
+      <br/>
+      <button>click me</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>a</Person>
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
     </div>
     )
   }
