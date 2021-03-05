51c72aae4ec9436fc448c427c63720f1d8f1ebdb
venkatfiniteloop <64524789+venkatfiniteloop@users.noreply.github.com>
2021-03-05 17:16:05 +0530

    Creating the div with React instead HTML. nested h1 shows in webpage

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import React,{Component} from 'react';
+//function App() {
+  // return (
+  //   <div className="App">
+  //     Hello World
+  //   </div>
+  // );
+//}
 
-function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+class App extends Component{
+  render(){
+    return React.createElement('div', null,'h1','Hello World123')
+  }
 }
-
 export default App;
