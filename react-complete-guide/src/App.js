7ecc87d05322b6587df3cfee8828ae083cb41a77
venkatfiniteloop <64524789+venkatfiniteloop@users.noreply.github.com>
2021-03-05 20:50:22 +0530

    Updating state with hooks. other state is missing while updating in 21 line

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,35 +1,34 @@
 import './App.css';
 import React,{Component} from 'react';
-import Person from './Person/Person'
+import Person from './Person/Person';
+import {useState} from 'react';
 
-
-class App extends Component{
-  state={
+const App=()=>{
+  const [personsState, setPersonsState]=useState({
     persons:[
       {name:'a',age:28},
       {name:'b',age:29},
       {name:'c',age:30}
     ],
     otherState:"Some value"
-  }
+  })
 
-  swichNameHandler=()=>{
+  const swichNameHandler=()=>{
     // This one dont works
     // this.state.persons=this.state.persons.map(a=>{a.age=a.age+1; return a})
-
-    this.setState(this.state.persons.map(a=>{a.age=a.age+1; return a}))
+  // debugger
+  // console.log();
+    setPersonsState({persons:personsState.persons.map(a=>{a.age=a.age+1; return a})})
   }
 
-  render(){
     return (<div className="App">
       Hello World
       <br/>
-      <button onClick={this.swichNameHandler}>click me</button>
-      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>a</Person>
-      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      <button onClick={swichNameHandler}>click me</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>a</Person>
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
     </div>
     )
-  }
 }
 export default App;
