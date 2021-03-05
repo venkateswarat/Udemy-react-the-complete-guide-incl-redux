456ec4cfd7315c33b4695f8b982298e4940396c4
venkatfiniteloop <64524789+venkatfiniteloop@users.noreply.github.com>
2021-03-05 21:52:05 +0530

    Passing method refs between components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,34 +1,35 @@
 import './App.css';
 import React,{Component} from 'react';
-import Person from './Person/Person';
-import {useState} from 'react';
+import Person from './Person/Person'
 
-const App=()=>{
-  const [personsState, setPersonsState]=useState({
+
+class App extends Component{
+  state={
     persons:[
       {name:'a',age:28},
       {name:'b',age:29},
       {name:'c',age:30}
     ],
     otherState:"Some value"
-  })
-  const [otherState, setOtherState]=useState("Some value")
-  const swichNameHandler=()=>{
-    // This one dont works
-    // this.state.persons=this.state.persons.map(a=>{a.age=a.age+1; return a})
-  // debugger
-  // console.log();
-    setPersonsState({persons:personsState.persons.map(a=>{a.age=a.age+1; return a})})
   }
 
+  swichNameHandler=()=>{
+    // This one dont works
+    // this.state.persons=this.state.persons.map(a=>{a.age=a.age+1; return a})
+
+    this.setState(this.state.persons.map(a=>{a.age=a.age+1; return a}))
+  }
+
+  render(){
     return (<div className="App">
       Hello World
       <br/>
-      <button onClick={swichNameHandler}>click me</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>a</Person>
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+      <button onClick={this.swichNameHandler}>click me</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>a</Person>
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.swichNameHandler}/>
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
     </div>
     )
+  }
 }
 export default App;
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react';
 const person =(props)=>{
     return <div>
-                <p>I am {props.name}. {props.age}  Years. addition {1+2}</p>
+                <p onClick={props.click}>I am {props.name}. {props.age}  Years. addition {1+2}</p>
                 <p>{props.children}</p>
             </div>
 }
