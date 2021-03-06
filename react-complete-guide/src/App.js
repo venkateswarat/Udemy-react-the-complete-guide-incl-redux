50ba9679ad0a506a74cc62d08ba7d2e7e5291795
venkatfiniteloop <64524789+venkatfiniteloop@users.noreply.github.com>
2021-03-06 13:22:08 +0530

    sudo css with Radium package

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React,{Component} from 'react';
 import Person from './Person/Person'
-
+import Radium from 'radium'
 
 class App extends Component{
   state={
@@ -37,7 +37,11 @@ class App extends Component{
       font:'inherit',
       border:'1px solid blue',
       padding:'8px',
-      cursor:'pointer'
+      cursor:'pointer',
+      ':hover':{
+        backgroundColor:'lightgreen',
+        color:'black'
+      }
     }
     let person=null;
     if(this.state.showPersons){
@@ -52,6 +56,10 @@ class App extends Component{
         </div>
       )
       style.backgroundColor='red';
+      style[':hover']={
+        backgroundColor:'lightred',
+        color:'black'
+      }
     }
     let classes=[];
     if(this.state.persons.length<=2){
@@ -70,4 +78,4 @@ class App extends Component{
     )
   }
 }
-export default App;
+export default Radium(App);
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Person.css'
+import Radium from 'radium'
+
 const person =(props)=>{
     return <div className="Person">
                 <p onClick={props.click}>I am {props.name}. {props.age}  Years. addition {1+2}</p>
@@ -7,4 +9,4 @@ const person =(props)=>{
                 <input type="text" onChange={props.changed} value={props.name}/>
             </div>
 }
-export default person;
+export default Radium(person);
