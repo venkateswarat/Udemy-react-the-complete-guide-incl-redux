52480b8524e304256e45d5230fef5d665c8aee92
venkatfiniteloop <64524789+venkatfiniteloop@users.noreply.github.com>
2021-03-06 13:50:05 +0530

    Styled Components

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import './Person.css'
+import styled from 'styled-components';
 
+const StyledDiv = styled.div`
+    width: 60%;
+    margin: auto;
+    border: 1px solid #eee;
+    box-shadow: 0 2px 3px #ccc;
+    padding: 16px;
+    text-align: center;
+
+    @media (min-width:500px){
+        width: 450px;
+    }
+`
 const person =(props)=>{
     const style={
         '@media(min-width:500px)':{
                 width: '450px'
         }
     }
-    return <div className="Person" style={style}>
+    return (   <StyledDiv>
                 <p onClick={props.click}>I am {props.name}. {props.age}  Years. addition {1+2}</p>
                 <p>{props.children}</p>
                 <input type="text" onChange={props.changed} value={props.name}/>
-            </div>
+                </StyledDiv>)
 }
 export default person;
