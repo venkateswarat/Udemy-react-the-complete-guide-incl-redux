e4a3dbcd0ae882a46fb7990b4508b6b97e6b2a40
venkatfiniteloop <64524789+venkatfiniteloop@users.noreply.github.com>
2021-03-09 19:41:41 +0530

    Fragment is from React, it does same as our Aux component

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React,{Component} from 'react';
+import React,{Component, Fragment} from 'react';
 import './Person.css'
 import styled from 'styled-components';
 import Aux from '../../../hoc/Auxliliary';
@@ -24,11 +24,11 @@ class Person extends Component{
                     width: '450px'
             }
         }
-        return (<Aux>
+        return (<Fragment>
                     <p  onClick={this.props.click}>I am {this.props.name}. {this.props.age}  Years. addition {1+2}</p>,
                     <p>{this.props.children}</p>,
                     <input   type="text" onChange={this.props.changed} value={this.props.name}/>
-                </Aux>);
+                </Fragment>);
                     
     }
     
