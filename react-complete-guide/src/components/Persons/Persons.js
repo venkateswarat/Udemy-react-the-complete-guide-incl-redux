cec3c021e089c4ee49e64145445bf6fcff1af3b8
venkatfiniteloop <64524789+venkatfiniteloop@users.noreply.github.com>
2021-03-09 18:09:24 +0530

    componentWillUnmount hook is used for clean up activities

--- react-complete-guide/src/components/Persons/Persons.js
+++ react-complete-guide/src/components/Persons/Persons.js
@@ -17,6 +17,9 @@ class Persons extends Component{
         console.log('[Persons.js] componentDidUpdate');
         console.log(snapshot);
     }
+    componentWillUnmount(){
+        console.log('[persons.js] componentWillUnmount')
+    }
     render(){
         console.log('[Persons.js] rendering...');
         return this.props.persons.map((person,index)=>{
