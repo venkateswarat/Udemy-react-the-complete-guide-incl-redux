986349678c12b003b3a46dd30f3468ea1d3e9251
venkatfiniteloop <64524789+venkatfiniteloop@users.noreply.github.com>
2021-03-09 19:10:35 +0530

    checking function references as well in

--- react-complete-guide/src/components/Persons/Persons.js
+++ react-complete-guide/src/components/Persons/Persons.js
@@ -7,12 +7,13 @@ class Persons extends Component{
     // }
     shouldComponentUpdate(nextProps, nextState){
         console.log('[Persons.js] shouldComponentUpdate')
-        /*if (nextProps.persons!==this.props.persons){
+        if (nextProps.persons!==this.props.persons||
+            nextProps.changed!==this.props.changed||
+            nextProps.clicked!==this.props.clicked){
             return true;
         }else{
             return false;
-        }*/
-        return true
+        }
     }
     getSnapshotBeforeUpdate(prevProps, prevState){
         console.log('[Persons.js] getSnapshotBeforeUpdate')
